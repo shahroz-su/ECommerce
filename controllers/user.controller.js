733e03c59abe65e1af96710bbed7e3986fa61046
shahroz-su <asad.ali.0072122@gmail.com>
2020-03-23 11:13:35 -0700

    Updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -145,7 +145,7 @@ exports.forget = (req, res, next) => {
         subject: 'Node.js Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/newpass/' + token + '\n\n' +
+          'https://mobie-store.herokuapp.com/' + '/newpass/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
       smtpTransport.sendMail(mailOptions, function(err) {
